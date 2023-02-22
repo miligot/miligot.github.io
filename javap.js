42000c2e01bf45ad85bb2edc47f66f4611b70b97
Milagros Gotthelf <milagrosjuanita@gmail.com>
2023-02-22 11:56:46 -0300

    first commit

---
+++ javap.js
@@ -0,0 +1,4 @@
+document.getElementById("boton").addEventListener("click", function(){
+    console.log("otros datos de intrés");
+    document.getElementById("demo").innerHTML = "Disponibilidad horaria de 8 horass"
+})
